a697328929c91e342a405573aad8c686f9f1c801
IagoAntunes <iagoantunes.f@gmail.com>
2022-09-17 22:35:18 -0300

    AppBar, PageView, BottomNavigation

--- lib/src/base/base_screen.dart
+++ lib/src/base/base_screen.dart
@@ -1,17 +1,49 @@
 import 'package:flutter/material.dart';
 
-class BaseScreen extends StatelessWidget {
+class BaseScreen extends StatefulWidget {
   const BaseScreen({super.key});
+
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  int currentIndex = 0;
+  final pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: Colors.red),
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: [
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.purple,
+          )
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.green,
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+              pageController.jumpToPage(currentIndex);
+            });
+          },
           selectedItemColor: Colors.white,
           unselectedItemColor: Colors.white.withAlpha(100),
           type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.green,
           items: const [
             BottomNavigationBarItem(
                 icon: Icon(Icons.home_outlined), label: 'Home'),
