0d972fb7cd0e9a53ebb66df36a10c00bb95ba6cf
IagoAntunes <iagoantunes.f@gmail.com>
2022-09-17 22:03:21 -0300

    Formatters TextField

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:quitandavirtual/src/auth/components/custom_text_field.dart';
 import 'package:quitandavirtual/src/auth/config/custom_colors.dart';
 
 class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({super.key});
+  SignUpScreen({super.key});
+
+  final cpfFormatter = MaskTextInputFormatter(
+      mask: '###.###.###-##', filter: {'#': RegExp(r'[0-9]')});
+  final phoneFormatter = MaskTextInputFormatter(
+      mask: '## # ####-####', filter: {'#': RegExp(r'[0-9]')});
 
   @override
   Widget build(BuildContext context) {
@@ -17,50 +23,74 @@ class SignUpScreen extends StatelessWidget {
         child: SizedBox(
           height: size.height,
           width: size.width,
-          child: Column(children: [
-            const Expanded(
-              child: Center(
-                child: Text(
-                  'Cadastro',
-                  style: TextStyle(color: Colors.white, fontSize: 35),
+          child: Stack(
+            children: [
+              Column(children: [
+                const Expanded(
+                  child: Center(
+                    child: Text(
+                      'Cadastro',
+                      style: TextStyle(color: Colors.white, fontSize: 35),
+                    ),
+                  ),
                 ),
-              ),
-            ),
 
-            //Form
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius:
-                      BorderRadius.vertical(top: Radius.circular(45))),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const CustomTextField(icon: Icons.email, label: 'Email'),
-                  const CustomTextField(
-                      icon: Icons.lock, label: 'Senha', isSecret: true),
-                  const CustomTextField(icon: Icons.person, label: 'Nome'),
-                  const CustomTextField(icon: Icons.phone, label: 'Celular'),
-                  const CustomTextField(icon: Icons.file_copy, label: 'Cpf'),
+                //Form
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(45))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const CustomTextField(icon: Icons.email, label: 'Email'),
+                      const CustomTextField(
+                          icon: Icons.lock, label: 'Senha', isSecret: true),
+                      const CustomTextField(icon: Icons.person, label: 'Nome'),
+                      CustomTextField(
+                          inputFormatters: [phoneFormatter],
+                          icon: Icons.phone,
+                          label: 'Celular'),
+                      CustomTextField(
+                          inputFormatters: [cpfFormatter],
+                          icon: Icons.file_copy,
+                          label: 'Cpf'),
 
-                  //Register Button
-                  SizedBox(
-                    height: 50,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18))),
-                        onPressed: () {},
-                        child: const Text(
-                          'Cadastrar Usuario',
-                          style: TextStyle(fontSize: 18),
-                        )),
-                  )
-                ],
-              ),
-            ),
-          ]),
+                      //Register Button
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18))),
+                            onPressed: () {},
+                            child: const Text(
+                              'Cadastrar Usuario',
+                              style: TextStyle(fontSize: 18),
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+              ]),
+              Positioned(
+                left: 10,
+                top: 10,
+                child: SafeArea(
+                    child: IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                        ))),
+              )
+            ],
+          ),
         ),
       ),
     );
