a26876535d8bb33a90b3c11fdaa1e0fcc8380df3
IagoAntunes <iagoantunes.f@gmail.com>
2022-09-18 02:19:43 -0300

    GridView Styles

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -1,54 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:quitandavirtual/src/config/custom_colors.dart';
 import 'package:quitandavirtual/src/models/item_model.dart';
+import 'package:quitandavirtual/src/services/utils_services.dart';
 
 class ItemTile extends StatelessWidget {
-  const ItemTile({
+  ItemTile({
     super.key,
     required this.item,
   });
 
   final ItemModel item;
+  final UtilsServices utilsServices = UtilsServices();
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1,
-      shadowColor: Colors.grey.shade300,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child:
-            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          //Imagem
-          Expanded(child: Image.asset(item.imgUrl)),
-          //Nome
-          Text(
-            item.itemName,
-            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    return Stack(
+      children: [
+        Card(
+          elevation: 1,
+          shadowColor: Colors.grey.shade300,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  //Imagem
+                  Expanded(child: Image.asset(item.imgUrl)),
+                  //Nome
+                  Text(
+                    item.itemName,
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  //Preço - unidade
+                  Row(
+                    children: [
+                      Text(
+                        utilsServices.priceTocurrency(item.price),
+                        style: TextStyle(
+                          color: CustomColors.customSwatchColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        '/${item.unit}',
+                        style: TextStyle(
+                            color: Colors.grey.shade500,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12),
+                      ),
+                    ],
+                  ),
+                ]),
           ),
-          //Preço - unidade
-          Row(
-            children: [
-              Text(
-                item.price.toString(),
-                style: TextStyle(
-                  color: CustomColors.customSwatchColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+        ),
+        Positioned(
+            top: 4,
+            right: 4,
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 40,
+                width: 35,
+                decoration: BoxDecoration(
+                    color: CustomColors.customSwatchColor,
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(15),
+                        topRight: Radius.circular(20))),
+                child: const Icon(
+                  Icons.add_shopping_cart_outlined,
+                  color: Colors.white,
+                  size: 20,
                 ),
               ),
-              Text(
-                '/${item.unit}',
-                style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 12),
-              ),
-            ],
-          ),
-        ]),
-      ),
+            ))
+      ],
     );
   }
 }
