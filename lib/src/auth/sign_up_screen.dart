363fcdbbcf2707d81d2a27464bb33aa44844c806
IagoAntunes <iagoantunes.f@gmail.com>
2022-09-17 21:51:29 -0300

    SignUp Screen

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:quitandavirtual/src/auth/components/custom_text_field.dart';
 import 'package:quitandavirtual/src/auth/config/custom_colors.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -8,26 +9,60 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: CustomColors.customSwatchColor,
-      body: Column(children: [
-        const Expanded(
-          child: Center(
-            child: Text(
-              'Cadastro',
-              style: TextStyle(color: Colors.white, fontSize: 35),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Column(children: [
+            const Expanded(
+              child: Center(
+                child: Text(
+                  'Cadastro',
+                  style: TextStyle(color: Colors.white, fontSize: 35),
+                ),
+              ),
             ),
-          ),
-        ),
 
-        //Form
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-          decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(top: Radius.circular(45))),
+            //Form
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(45))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const CustomTextField(icon: Icons.email, label: 'Email'),
+                  const CustomTextField(
+                      icon: Icons.lock, label: 'Senha', isSecret: true),
+                  const CustomTextField(icon: Icons.person, label: 'Nome'),
+                  const CustomTextField(icon: Icons.phone, label: 'Celular'),
+                  const CustomTextField(icon: Icons.file_copy, label: 'Cpf'),
+
+                  //Register Button
+                  SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18))),
+                        onPressed: () {},
+                        child: const Text(
+                          'Cadastrar Usuario',
+                          style: TextStyle(fontSize: 18),
+                        )),
+                  )
+                ],
+              ),
+            ),
+          ]),
         ),
-      ]),
+      ),
     );
   }
 }
