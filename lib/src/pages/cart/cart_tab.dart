b4c327b075c332f4d35293725c0638455a3e3b5b
IagoAntunes <iagoantunes.f@gmail.com>
2022-10-18 11:28:12 -0300

    AlertDialog

--- lib/src/pages/cart/cart_tab.dart
+++ lib/src/pages/cart/cart_tab.dart
@@ -69,7 +69,9 @@ class _CartTabState extends State<CartTab> {
             SizedBox(
               height: 50,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () async {
+                  bool? result = await showOrderConfirmation();
+                },
                 style: ElevatedButton.styleFrom(
                     backgroundColor: CustomColors.customSwatchColor,
                     shape: RoundedRectangleBorder(
@@ -83,6 +85,38 @@ class _CartTabState extends State<CartTab> {
           ]),
         ),
       ]),
+    );
+  }
+
+  Future<bool?> showOrderConfirmation() {
+    return showDialog<bool>(
+      context: context,
+      builder: ((context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          title: const Text('Confirmação'),
+          content: const Text('Deseja Realmente concluir o pedido ?'),
+          actions: [
+            TextButton(
+              onPressed: (() {
+                Navigator.of(context).pop(false);
+              }),
+              child: Text('Não'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+              onPressed: (() {
+                Navigator.of(context).pop(true);
+              }),
+              child: Text('Sim'),
+            )
+          ],
+        );
+      }),
     );
   }
 }
